cac889abe5edebfed3a93ed6dea4437ae354dcef
Soham Bhunia <sohambhunia2001@gmail.com>
2024-12-13 10:38:56 +0530

    fix

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useAuthStore } from "../store/useAuthStore";
-import { Camera, Mail, User } from "lucide-react";
+import { Camera, Mail, User } from "lucide-react"; //
 
 const ProfilePage = () => {
   const { authUser, isUpdatingProfile, updateProfile } = useAuthStore();
