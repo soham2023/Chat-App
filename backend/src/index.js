a4819ca5b7e05eab60852f4152ba661e27f9bf41
Soham Bhunia <sohambhunia2001@gmail.com>
2024-12-13 14:36:57 +0530

    fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use( 
     cors({
-        origin: "http://localhost:5050",
+        origin: "http://localhost:3030",
         credentials:true,
     })
 );
